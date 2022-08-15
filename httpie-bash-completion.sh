9fe31bdce74e5635f90a9c6e02e8316107396fc8
Hyunho Cho <mug896@gmail.com>
2022-08-15 18:19:28 +0900

    update

--- httpie-bash-completion.sh
+++ httpie-bash-completion.sh
@@ -1,32 +1,30 @@
 _http () 
 {
-    local CMD=${COMP_WORDS[0]}
-    local CUR=${COMP_WORDS[COMP_CWORD]}
-    local PREV=${COMP_WORDS[COMP_CWORD-1]}
+    local CMD=$1 CUR=$2 PREV=$3
     local IFS=$' \t\n' WORDS _CMD=__$CMD
     local VER=$(stat -L -c %Y `type -P "$CMD"`)
     local HELP=${!_CMD#*$'\n'}
 
-    [ "$PREV" = "=" ] && PREV=${COMP_WORDS[COMP_CWORD-2]}
-    if [ "${CUR:0:1}" = "-" ]; then
-        if [ -z "${!_CMD}" -o "$VER" != "${!_CMD%%$'\n'*}" ]; then
+    [[ $PREV = "=" ]] && PREV=${COMP_WORDS[COMP_CWORD-2]}
+    if [[ ${CUR:0:1} = "-" ]]; then
+        if [[ -z ${!_CMD} || $VER != ${!_CMD%%$'\n'*} ]]; then
             eval ${_CMD}='$VER$'"'\\n'"'$( $CMD --help )'
         fi
         WORDS=$(echo "${!_CMD#*$'\n'}" | sed -En '/^  -/p' | grep -Eo -- ' -[[:alnum:]-]+\b')
-    elif [ "$PREV" = "--ssl" ]; then
+    elif [[ $PREV = --ssl ]]; then
         WORDS=$(echo "$HELP" | sed -En '/^  --ssl/{ s/^[^{]*(.*)}.*/\1/; s/,|\{|}/ /g; p }')
-    elif [ "$PREV" = "--auth-type" -o "$PREV" = "-A" ]; then
+    elif [[ $PREV = @(-A|--auth-type) ]]; then
         WORDS="basic bearer digest"
-    elif [ "$PREV" = "--print" -o "$PREV" = "-p" ]; then
+    elif [[ $PREV = @(-p|--print) ]]; then
         IFS=$'\n'
         WORDS='\"H\" request headers
 \"B\" request body
 \"h\" response headers
 \"b\" response body
 \"m\" response metadata'
-    elif [ "$PREV" = "--pretty" ]; then
+    elif [[ $PREV = --pretty ]]; then
         WORDS="all colors format none"
-    elif [ "$PREV" = "--style" -o "$PREV" = "-s" ]; then
+    elif [[ $PREV = @(-s|--style) ]]; then
         WORDS="abap algol algol_nu arduino auto autumn borland bw
           colorful default dracula emacs friendly
           friendly_grayscale fruity gruvbox-dark gruvbox-light
@@ -36,16 +34,17 @@ _http ()
           rrt sas solarized solarized-dark solarized-light stata
           stata-dark stata-light tango trac vim vs xcode
           zenburn"
-    elif [[ "${CUR:0:1}" != @(\'|\"|=) ]]; then
+    elif [[ $PREV = @(-o|--output) ]]; then
+        compopt -o filenames
+    else
         WORDS="GET POST PUT HEAD DELETE PATCH OPTIONS CONNECT TRACE"
     fi
-    [ "$CUR" = "=" ] && CUR=""
     COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
 }
 
 _httpie () 
 {
-    local CUR=${COMP_WORDS[COMP_CWORD]}
+    local CUR=$2
     local IFS=$' \t\n' WORDS HELP
     HELP=$( eval "${COMP_LINE% *} --help" 2>&1 ) || return;
 
